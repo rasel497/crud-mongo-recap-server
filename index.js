4711b749b5319eb11ec789c79ee3000b97679936
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-07 12:23:41 +0600

    server side initial setp done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('crud mongoDB recup server running');
+})
+
+app.listen(port, () => {
+    console.log(`mongoDB server running on port: ${port}`);
+})
+
+
+
