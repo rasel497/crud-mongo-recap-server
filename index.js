5b53d279b84a6eb8071fcd4ac32c8a2fb4df8556
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-07 14:27:07 +0600

    wow first insertOn product send to databse

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { yellow } = require('colors');
 require('dotenv').config();
-
+require("colors"); //optional
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,13 +13,33 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mpr3cem.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database connected'.yellow.italic);
+
+        const productCollection = client.db("crudMongoDbRecup").collection("products");
+        const userCollection = client.db("crudMongoDbRecup").collection("users");
+
+        productCollection.insertOne({ name: 'Pizza' });
+    }
+    catch (error) {
+        // console.log(error.name, error.message, error.stack);
+        console.log(error.name.bgRed, error.message.bold, error.stack);
+    }
+}
+run();
+
+
+
 app.get('/', (req, res) => {
     res.send('crud mongoDB recup server running');
 })
 
 app.listen(port, () => {
-    console.log(`mongoDB server running on port: ${port}`);
+    console.log(`mongoDB server running on port: ${port}`.cyan.bold);
 })
-
-
-
